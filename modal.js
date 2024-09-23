aa38bcd0adf91c162074853fd6da3c22b0a555a6
JUNIOR SOUZA <junior.1937@aluno.pr.senac.br>
2024-09-23 11:58:16 -0300

    versao 23-09-2024

--- modal.js
+++ modal.js
@@ -1,14 +1,23 @@
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("openModal");
-const span = document.getElementsByClassName("close")[0];
-btn.onclick = function () {
-    modal.style.display = "block"; // Exibe a modal
-}
-span.onclick = function () {
-    modal.style.display = "none"; // Fecha a modal
-}
-window.onclick = function (event) {
-    if (event.target === modal) {
-        modal.style.display = "none"; // Fecha se clicar fora da modal
-    }
-}
+const modals = document.querySelectorAll(".modal");
+const btn = document.querySelectorAll(".openModal");
+const span = document.querySelectorAll(".close");
+
+btn.forEach((button, index) => {
+    button.onclick = function() {
+        modals[index].style.display = "block"; // Exibe a modal
+    };
+});
+
+span.forEach((button, index) => {
+    span.onclick = function() {
+        modals[index].style.display = "none"; // Fecha a modal
+    };
+});
+window.onclick = function(event) {
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = "none"; // Fecha se clicar fora da modal
+        }
+
+    });
+};
